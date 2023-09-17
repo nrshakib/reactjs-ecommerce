10a6e32d10345ad683834c7ad6c1911ff1f6f9e0
NRS\nrsha <nrshakib007@gmail.com>
2023-09-18 01:14:22 +0600

    Navbar Design Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 lg:px-14 xl:px-14 ">
+    <div className="navbar bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% lg:px-14 xl:px-14 ">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           <img src={logo} alt="e-commerce" />
@@ -44,16 +44,22 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="navbar-center hidden lg:flex md:flex lg:mr-10">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to="/products">Products</Link>
+      <div className="navbar-center hidden lg:flex md:flex lg:mr-56 lg:text-gray-800 font-medium ">
+        <ul className="menu menu-horizontal px-1 text-xl ">
+          <li className="hover:bg-sky-300 hover:rounded-lg focus:ring-violet-500">
+            <Link className="hover:text-black" to="/products">
+              Products
+            </Link>
           </li>
-          <li>
-            <Link to="/cart">Cart</Link>
+          <li className="hover:bg-sky-300 hover:rounded-lg">
+            <Link className="hover:text-black" to="/cart">
+              Cart
+            </Link>
           </li>
-          <li>
-            <Link to="/offers">Offers</Link>
+          <li className="hover:bg-sky-300 hover:rounded-lg ">
+            <Link className="hover:text-black " to="/offers">
+              Offers
+            </Link>
           </li>
         </ul>
       </div>
@@ -66,8 +72,8 @@ const Navbar = () => {
           />
         </div>
         <div>
-          <Link to="/login" className="flex">
-            <p className="mr-1">Log In</p>
+          <Link to="/login" className="flex  text-gray-800 ">
+            <p className="mr-1 font-medium">Log In</p>
             <AiOutlineLogin className="mt-1" />
           </Link>
           <div className="dropdown dropdown-end">
